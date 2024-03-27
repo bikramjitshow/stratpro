5784b64a762fa6bcda383312809145738ba61c28
Bikramjit <bikramjitshow2016@gmail.com>
2024-03-27 22:49:45 +0530

    add

--- demo_one/sc-casa-account-selector/sc-casa-account-selector copy.js
+++ demo_one/sc-casa-account-selector/sc-casa-account-selector copy.js
@@ -11,11 +11,6 @@ class AccountSelector {
     // Set initial style to "none"
     this.myButton.style.display = "none";
 
-    this.myButton.addEventListener("click", () => {
-      console.log("Scrool to top");
-      this.topFunction();
-    });
-
     // When the user scrolls down 250px from the top of the document, show the button
     window.onscroll = () => this.scrollFunction();
 
@@ -106,25 +101,20 @@ class AccountSelector {
     // this.setHeightSameTag(deviceWidth);
     // DOMContentLoaded
     document.addEventListener("DOMContentLoaded", () => {
-      limit = document
-        .querySelector(".sc-casa-product-list")
-        .getAttribute("data-limit");
-
       // Event listener for category clicks
       document
         .querySelectorAll(".sc-casa-product-list__category")
         .forEach((category) => {
+          limit = category.getAttribute("data-limit");
+          // console.log("DOMContentLoaded LOAD CALLED !!", limit);
           // Initialize by showing the first tab and first 6 offers
           this.showTab("all-accounts", limit, deviceWidth);
           currentFilter = "all-accounts";
-
-          //each category clicks
           category.addEventListener("click", (e) => {
             e.preventDefault();
             const filter = category.getAttribute("data-filter");
             currentFilter = filter;
             // this.setHeightSameTag(deviceWidth, currentFilter);
-            this.topFunction();
             this.showTab(filter, limit, deviceWidth);
           });
         });
@@ -155,8 +145,7 @@ class AccountSelector {
 
   // Implementation for same height for row base cards
   setHeightSameTag(w, f) {
-    // console.log("filter data--->>>>:", f);
-    // console.log("card view:", w);
+    console.log("filter data:", f);
 
     // Get all the rows of card columns
     const rows = document.querySelectorAll(".sc-casa-product-list__row");
@@ -167,12 +156,12 @@ class AccountSelector {
       // );
       const cards = Array.from(f);
 
-      // console.log("filter data:", cards);
+      console.log("filter data:", cards);
 
       // Loop through the cards in groups of three
       for (let i = 0; i < cards.length; i += w) {
         const group = cards.slice(i, i + w); // Get a group of three cards
-        // console.log("filter group->", group);
+        console.log("filter group->", group);
 
         // Calculate the maximum title height for the current group
         let maxGreenWrapperHeight = 0;
@@ -197,6 +186,7 @@ class AccountSelector {
             }
             if (title) {
               maxTitleHeight = Math.max(maxTitleHeight, title.offsetHeight);
+              console.log("maxTitleHeight", maxTitleHeight);
             }
             if (description) {
               maxDescriptionHeight = Math.max(
@@ -217,8 +207,8 @@ class AccountSelector {
             const description = card.querySelector(
               ".sc-casa-product-list__description"
             );
-            // console.log("filter data title:", card.style.display);
-            // console.log("filter data clientheight:", title.offsetHeight);
+            console.log("filter data title:", card.style.display);
+            console.log("filter data clientheight:", title.offsetHeight);
             if (greenWrapper) {
               greenWrapper.style.height = `${maxGreenWrapperHeight}px`;
             }
@@ -241,10 +231,10 @@ class AccountSelector {
     );
     // Hide all product boxes
     document
-      .querySelectorAll(".sc-casa-product-list__col")
+      .querySelectorAll(".sc-casa-product-list__card-box")
       .forEach(function (box) {
         box.style.display = "none";
-        // box.parentNode.style.display = "none";
+        box.parentNode.style.display = "none";
       });
 
     // Show the selected category
@@ -256,39 +246,32 @@ class AccountSelector {
       categories.forEach((category) => {
         const filter = category.getAttribute("data-filter");
         let allFilter2 = document.querySelectorAll(
-          ".sc-casa-product-list__col[data-tags*='" + filter + "']"
+          ".sc-casa-product-list__card-box[data-tags*='" + filter + "']"
         );
         allFilter.push(...allFilter2);
-        let uniqData = new Set(allFilter);
-        let filteredData = [...uniqData];
         setTimeout(() => {
-          this.setHeightSameTag(deviceWidth, filteredData);
+          this.setHeightSameTag(deviceWidth, allFilter);
         }, 500);
       });
       // console.log("showtab Filter", filter);
       // console.log("showtab allFilter", allFilter);
     } else {
       allFilter = document.querySelectorAll(
-        ".sc-casa-product-list__col[data-tags*='" + filter + "']"
+        ".sc-casa-product-list__card-box[data-tags*='" + filter + "']"
       );
-      let uniqData = new Set(allFilter);
-      let filteredData = [...uniqData];
       setTimeout(() => {
-        this.setHeightSameTag(deviceWidth, filteredData);
+        this.setHeightSameTag(deviceWidth, allFilter);
       }, 500);
       // console.log("showtab Filter", filter);
       // console.log("showtab allFilter", allFilter);
     }
 
-    // console.log("limit-->", limit);
-    let uniqData = new Set(allFilter);
-    let filteredData = [...uniqData];
-    // console.log("showtab Filter", filteredData);
-    filteredData.forEach(function (box, index) {
+    console.log("limit-->", limit);
+    allFilter.forEach(function (box, index) {
       box.style.display = index < limit ? "flex" : "none";
-      // box.parentNode.style.display = index < limit ? "flex" : "none";
+      box.parentNode.style.display = index < limit ? "flex" : "none";
     });
-    loadMoreBtn.style.display = filteredData.length > limit ? "flex" : "none";
+    loadMoreBtn.style.display = allFilter.length > limit ? "flex" : "none";
 
     // Update active tag
     document
@@ -310,44 +293,33 @@ class AccountSelector {
   loadMore(currentFilter, deviceWidth) {
     let that = this;
     var visibleCount = document.querySelectorAll(
-      ".sc-casa-product-list__col:not([style*='none'])"
+      ".sc-casa-product-list__card-box:not([style*='none'])"
     ).length;
 
     console.log("currentFilter -->", currentFilter);
     let allFilter = document.querySelectorAll(
-      ".sc-casa-product-list__col"
+      ".sc-casa-product-list__card-box"
     );
-    let uniqData = new Set(allFilter);
-    let filteredData = [...uniqData];
     setTimeout(() => {
-      that.setHeightSameTag(deviceWidth, filteredData);
+      that.setHeightSameTag(deviceWidth, allFilter);
     }, 500);
     // Show the next 10 offers
     document
-      .querySelectorAll(".sc-casa-product-list__col")
+      .querySelectorAll(".sc-casa-product-list__card-box")
       .forEach(function (box, index) {
-        // console.log("currentFilter--", currentFilter);
-        // console.log(
-        //   "box tags--",
-        //   box.getAttribute("data-tags").includes(currentFilter)
-        // );
-        if (
-          currentFilter === "all-accounts" ||
-          box.getAttribute("data-tags").includes(currentFilter)
-        ) {
+        // box.style.display = "flex";
+        // box.parentNode.style.display = "flex";
+        if (currentFilter === "all-accounts") {
           box.style.display = "flex";
-          // box.parentNode.style.display = "flex";
-          if (
-            currentFilter !== "other-products" &&
-            box.getAttribute("data-tags") === "other-products"
-          ) {
+          box.parentNode.style.display = "flex";
+          if (box.getAttribute("data-tags") === "other-products") {
             box.style.display = "none";
-            // box.parentNode.style.display = "none";
+            box.parentNode.style.display = "none";
           }
         } else if (currentFilter === box.getAttribute("data-tags")) {
-          console.log("box==", box);
+          console.log(box);
           box.style.display = "flex";
-          // box.parentNode.style.display = "flex";
+          box.parentNode.style.display = "flex";
         }
       });
   }
